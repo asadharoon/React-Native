6baaa553b45f483418399ff5b558e26775627e43
Asad Haroon <45485946+asadharoon@users.noreply.github.com>
2020-01-18 16:50:55 +0500

    React Native Lec1
    State Toggle,Props,Functions,Styles,Text,View,Button

---
+++ Tutorials/Lec#1- Basics(Button,Text,State,Styles)/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [name, setName] = useState("Asad");
+
+  const [isToggle, setToggle] = useState(false);
+  const onButtonClick = () => {
+    console.log(isToggle);
+    if (!isToggle == true) {
+      setName("Changed Text");
+      setToggle(true);
+    } else {
+      console.log("button clicked");
+      setName("Asad");
+      setToggle(false);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.heading}>Hello Asad to Lesson 1 by NetNinja</Text>
+        <Button title="Change State" onPress={onButtonClick}></Button>
+        {/* Another way of Setting function onPress to the button  */}
+        {/* <Button title="Change State" onPress={e=> onButtonClick()}></Button> */}
+      </View>
+      <Text>My name is: {name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20
+  },
+  heading: {
+    fontWeight: "bold"
+  }
+});
